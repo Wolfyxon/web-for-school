6f2f7956ca114ece6f41008ae88744e9b2a654f5
Wolfyxon <wolfyxon@gmail.com>
2024-03-18 17:49:31 +0100

    Added missing description

--- highSchool/2/PAI/18.03.2024/btnEvent/script.js
+++ highSchool/2/PAI/18.03.2024/btnEvent/script.js
@@ -1,3 +1,7 @@
+/*
+1. Napisz kod, który po naciśnięciu przycisku z podpisem Powitaj!. wyświetli tekst - nagłówek 1 stopnia - Witaj świecie! a sam przycisk zniknie.
+*/
+
 window.addEventListener("load", () => {
     const btn = document.getElementById("btn-welcome");
     const header = document.getElementById("welcome-header");
