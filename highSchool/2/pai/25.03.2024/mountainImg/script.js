76c1975279e7cc1f17194b897875a85ab8b4e00c
Wolfyxon <wolfyxon@gmail.com>
2024-04-02 18:59:27 +0200

    Simpler solution for storing image options

--- highSchool/2/pai/25.03.2024/mountainImg/script.js
+++ highSchool/2/pai/25.03.2024/mountainImg/script.js
@@ -7,24 +7,6 @@ window.addEventListener("load", () => {
     const dropdown = document.getElementById("dropdown");
     const img = document.getElementById("img");
 
-    /**
-     * @param {String} name 
-     * @param {String} src 
-     */
-    function registerImg(name, src) {
-        const option = document.createElement("option");
-
-        option.innerText = name;
-        option.value = src;
-
-        dropdown.appendChild(option);
-    }
-
-    registerImg("Wybierz łańcuch górski", "https://www.jscwiczenia.pl/pliki/grafika/wybierz.jpg");
-    registerImg("Tatry - Rysy", "https://www.jscwiczenia.pl/pliki/grafika/rysy.jpg");
-    registerImg("Karkonosze - Śnieżka", "https://www.jscwiczenia.pl/pliki/grafika/sniezka.jpg");
-    registerImg("Beskidy - Babia Góra", "https://www.jscwiczenia.pl/pliki/grafika/babia.jpg");
-
     function update() {
         img.src = dropdown.value;
     }
