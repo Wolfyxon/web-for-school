4f987847ca3fc4d9392de0053c185149ace144e5
Wolfyxon <wolfyxon@gmail.com>
2024-04-14 23:37:46 +0200

    Added missing description

--- highSchool/2/pai/08.04.2024/yellow/script.js
+++ highSchool/2/pai/08.04.2024/yellow/script.js
@@ -1,3 +1,7 @@
+/*
+Napisz skrypt, który będzie powodował, że naciśnięcie przycisku z napisem Zmień tło będzie zmieniało kolor tła na żółty. 
+*/
+
 window.addEventListener("load", () => {
     document.getElementById("btn-set").addEventListener("click", () => {
         document.body.style.backgroundColor = "yellow";
