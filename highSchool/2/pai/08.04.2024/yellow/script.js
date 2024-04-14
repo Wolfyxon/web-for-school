b73641c7922b970a3e738394b6df6a6e1e80f959
Wolfyxon <wolfyxon@gmail.com>
2024-04-14 23:21:17 +0200

    Init tasks for 08.04.2024, added 'yellow'

---
+++ highSchool/2/pai/08.04.2024/yellow/script.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", () => {
+    document.getElementById("btn-set").addEventListener("click", () => {
+        document.body.style.backgroundColor = "yellow";
+    });
+});
