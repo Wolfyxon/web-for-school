0f2bed5f98392176201a4bf8793a4e1b7ec75b10
Wolfyxon <wolfyxon@gmail.com>
2024-04-28 11:56:23 +0200

    Added 11_tblSort

---
+++ highSchool/2/pai/15.04.2024/11_tblSort.js
@@ -0,0 +1,12 @@
+/*
+    Zdefiniuj prostą tablicę, która będzie zawierała liczby: 'Zenek', 'Tomasz', 'Adam', 'Ewa', 'Krzysztof'. 
+    Posortuj ją, ponownie ją wyświetl.
+*/
+
+const tbl = ["Zenek", "Tomasz", "Adam", "Ewa", "Krzysztof"];
+
+console.log(tbl);
+
+tbl.sort();
+
+console.log(tbl);
