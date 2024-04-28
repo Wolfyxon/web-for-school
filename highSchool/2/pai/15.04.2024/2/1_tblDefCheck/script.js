d8e4a12725b5ac90b345bbf77601b0a357ef5815
Wolfyxon <wolfyxon@gmail.com>
2024-04-28 12:19:25 +0200

    Init 2nd part, added 1_tblDefCheck

---
+++ highSchool/2/pai/15.04.2024/2/1_tblDefCheck/script.js
@@ -0,0 +1,14 @@
+/*
+    Sprawdź czy definicja osoby=["Jan Nowak","Kazimierz Zyga","Stefan Koc","Ewa Mocek","Mariusz Abramski"] to tablica, 
+    jeśli tak wyświetl na stronie tekst: "osoby to tablica" jeśli nie: "osoby to nie tablica". 
+*/
+
+const osoby = ["Jan Nowak","Kazimierz Zyga","Stefan Koc","Ewa Mocek","Mariusz Abramski"]
+
+window.addEventListener("load", () => {
+    if( Array.isArray(osoby) ) {
+        document.write("osoby to tablica");
+    } else {
+        document.write("osoby to nie tablica");
+    }
+});
