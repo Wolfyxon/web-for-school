cae74cd8d197aa7bf3451f5fdd25e67b7ecaca0f
Wolfyxon <wolfyxon@gmail.com>
2024-04-28 11:52:39 +0200

    Added 10_tblRev

---
+++ highSchool/2/pai/15.04.2024/10_tblRev.js
@@ -0,0 +1,12 @@
+/*
+    Zdefiniuj prostą tablicę, która będzie zawierała liczby: 5,12,17,23,123,45,54. 
+    Wypisz tablicę do do konsoli. Odwróć zawartość tablicy i ponownie ją wyświetl.
+*/
+
+const tbl = [5, 12, 17, 23, 123, 45, 54];
+
+console.log(tbl);
+
+tbl.reverse();
+
+console.log(tbl);
