490564ee9f11d1fae574bed8eafbd546d9a3e0f7
Wolfyxon <wolfyxon@gmail.com>
2024-04-28 12:30:54 +0200

    Added 4_tblNames

---
+++ highSchool/2/pai/15.04.2024/2/4_tblNames.js
@@ -0,0 +1,13 @@
+/*
+    Na podstawie danej tablicy osoby=["Jan Nowak","Kazimierz Zyga","Stefan Koc","Ewa Mocek","Mariusz Abramski"]. 
+    utwórz nową tablicę osobyImiona, która będzie zawierała tylko imiona osób z tablicy osoby.
+*/
+
+const osoby = ["Jan Nowak","Kazimierz Zyga","Stefan Koc","Ewa Mocek","Mariusz Abramski"];
+const osobyImiona = [];
+
+for(const v of osoby) {
+    osobyImiona.push( v.split(" ")[0] );
+}
+
+console.log(osobyImiona);
