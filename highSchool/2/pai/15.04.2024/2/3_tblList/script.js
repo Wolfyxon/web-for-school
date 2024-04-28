350d352ead0f8acf6b779ac02472fa17f25b77e3
Wolfyxon <wolfyxon@gmail.com>
2024-04-28 12:29:00 +0200

    Added 3_tblList

---
+++ highSchool/2/pai/15.04.2024/2/3_tblList/script.js
@@ -0,0 +1,20 @@
+/*
+    Wykorzystaj podaną tablicę osoby=["Jan Nowak","Kazimierz Zyga","Stefan Koc","Ewa Mocek","Mariusz Abramski"]. 
+    Wyświetl jej elementy w jedenj lini rozdzielone przecinakmi. Poniżej wyświetl jej elementy w kolejnych akapitach. 
+*/
+
+const osoby = ["Jan Nowak","Kazimierz Zyga","Stefan Koc","Ewa Mocek","Mariusz Abramski"];
+
+window.addEventListener("load", () => {
+    document.write(osoby.join(", "));
+
+    const frag = document.createDocumentFragment();
+
+    for(const v of osoby) {
+        const p = document.createElement("p");
+        p.innerText = v;
+        frag.appendChild(p);
+    }
+
+    document.body.appendChild(frag);
+});
