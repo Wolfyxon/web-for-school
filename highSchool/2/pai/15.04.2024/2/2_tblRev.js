cddf14d02df00426ffb6769344eee4d04ee584b9
Wolfyxon <wolfyxon@gmail.com>
2024-04-28 12:21:07 +0200

    Added 2_tblRev

---
+++ highSchool/2/pai/15.04.2024/2/2_tblRev.js
@@ -0,0 +1,10 @@
+/*
+    Wykorzystaj podaną tablicę osoby=["Jan Nowak","Kazimierz Zyga","Stefan Koc","Ewa Mocek","Mariusz Abramski"] 
+    Odwróć tablicę, pierwszy element ma być ostatnim, itd. 
+*/
+
+const osoby = ["Jan Nowak","Kazimierz Zyga","Stefan Koc","Ewa Mocek","Mariusz Abramski"];
+
+osoby.reverse();
+
+console.log(osoby);
