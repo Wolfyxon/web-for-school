d53bfaf45641d32c3d5c31bbbaa806b7030f5e50
Wolfyxon <wolfyxon@gmail.com>
2024-04-28 11:34:27 +0200

    Added 5_tblPush

---
+++ highSchool/2/pai/15.04.2024/5_tblPush.js
@@ -0,0 +1,12 @@
+/*
+    Zdefiniuj prostą tablicę, która będzie zawierała liczby: 5,12,17,23,123,45,54. 
+    Wypisz tablicę do do konsoli. Dodaj do tablicy 2 liczby 201 i 1. Ponownie tablicę do konsoli.
+*/
+
+const tbl = [5, 12, 17, 23, 123, 45, 54];
+
+console.log(tbl);
+
+tbl.push(201, 1);
+
+console.log(tbl);
