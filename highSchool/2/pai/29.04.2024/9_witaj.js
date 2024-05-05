de25f3977c89d5530dad4150ac4ff6f99210a0e6
Wolfyxon <wolfyxon@gmail.com>
2024-05-05 17:19:19 +0200

    Added 9_witaj

---
+++ highSchool/2/pai/29.04.2024/9_witaj.js
@@ -0,0 +1,13 @@
+/*
+    Zdefiniuj wyrażenie funkcyjne o nazwie witaj(), 
+    które wypisuje do konsoli tekst "Witaj świecie!". 
+    Wywołaj je po definicji wyrażenia a potem przed. 
+*/
+
+witaj();
+
+function witaj() {
+    console.log("Witaj świecie!");
+}
+
+witaj();
