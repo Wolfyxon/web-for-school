e2c11ede3fa6d97ac761383701d465596c10d92e
Wolfyxon <wolfyxon@gmail.com>
2024-05-05 17:27:42 +0200

    Added 11_rectAreaArrow

---
+++ highSchool/2/pai/29.04.2024/11_rectAreaArrow.js
@@ -0,0 +1,16 @@
+/*
+    Utwórz wyrażenie funkcyjne poleProstokata, podawane są dwa argumenty a i b. 
+    Wynik ma być wypisywany do konsoli. 
+    Skorzystaj z tzw. funkcji strzałkowych. Wywołaj funkcję z argumentami 2 i 5 oraz 20 i 50. 
+*/
+
+/**
+ * @param {number} a 
+ * @param {number} b 
+ */
+const poleProstokata = (a, b) => {
+    console.log(a * b);
+};
+
+poleProstokata(2, 5);
+poleProstokata(20, 50);
