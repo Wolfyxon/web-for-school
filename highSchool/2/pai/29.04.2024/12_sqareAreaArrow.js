cbc4d2b0ddc10cc5469f8f6e9c04f29769816ab5
Wolfyxon <wolfyxon@gmail.com>
2024-05-05 17:30:10 +0200

    Added 12_squareAreaArrow

---
+++ highSchool/2/pai/29.04.2024/12_sqareAreaArrow.js
@@ -0,0 +1,14 @@
+/*
+Utwórz wyrażenie funkcyjne poleKwadratu, argumentem jest bok a. 
+Wynik ma być wypisywany do konsoli. Skorzystaj z tzw. funkcji strzałkowych. Wywołaj funkcję z argumentami 5 oraz 50. 
+*/
+
+/**
+ * @param {number} a 
+ */
+const poleKwadratu = (a) => {
+    console.log(a ** 2);
+};
+
+poleKwadratu(5);
+poleKwadratu(50);
