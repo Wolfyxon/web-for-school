3021855e46e2ea05056225909200a990c22128b6
Wolfyxon <wolfyxon@gmail.com>
2024-10-15 13:48:45 +0200

    Coloring only <p> elements

--- highSchool/3/ZaawansowaneAplikacjeWebowe/17.10.2024/zad2/script.js
+++ highSchool/3/ZaawansowaneAplikacjeWebowe/17.10.2024/zad2/script.js
@@ -10,6 +10,8 @@ $(document).ready(() => {
 
     $("#btn-change").click(() => {
         for(const elm of $("body *")) {
+            if(elm.tagName != "P") continue;
+            
             $(elm).css("color", randColor());
         }
     })
