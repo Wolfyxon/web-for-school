6198553fcfd7f3f51adf29207327259c9255b9da
Wolfyxon <wolfyxon@gmail.com>
2024-11-06 10:08:15 +0100

    Forgor semicolon

--- highSchool/3/ProgramowanieZaawansowanychAplikacjiWebowych/23.10.2024/io.js
+++ highSchool/3/ProgramowanieZaawansowanychAplikacjiWebowych/23.10.2024/io.js
@@ -1,7 +1,7 @@
 async function input(query) {
     return new Promise((res) => {
         process.stdout.write(query);
-        process.stdin.resume()
+        process.stdin.resume();
 
         process.stdin.once("data", (data) => {
             process.stdin.pause();
