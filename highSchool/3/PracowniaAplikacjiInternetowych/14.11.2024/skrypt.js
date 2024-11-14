3a359fde2c80cf309dc7fae960a9fb3d7cbecaf2
Wolfyxon <wolfyxon@gmail.com>
2024-11-14 07:30:22 +0100

    Init script

--- highSchool/3/PracowniaAplikacjiInternetowych/14.11.2024/skrypt.js
+++ highSchool/3/PracowniaAplikacjiInternetowych/14.11.2024/skrypt.js
@@ -1,10 +1,42 @@
-const foto = document.querySelectorAll('.foto');
-const kontener = document.querySelector('#kontener');
-function f5(){
-	kontener.innerHTML='';
-	for(const i of foto){
-		if(i.checked){
-			kontener.innerHTML+=`<img src="${i.value}" />`;
+window.addEventListener("load", () => {
+	const gallery = document.getElementById("galeria");
+	const imgInput = document.getElementById("current-img");
+	const brdColorInput = document.getElementById("img-border-color")
+	const widthInput = document.getElementById("img-width");
+
+	function getCurrentImg() {
+		for(const img of gallery.children) {
+			if(img.src.endsWith(imgInput.value)) {
+				return img;
+			}
 		}
 	}
-}
+
+	document.getElementById("btn-apply-border-color").addEventListener("click", () => {
+		for(const img of gallery.children) {
+			img.style.borderColor = brdColorInput.value;
+		}
+	});
+
+	document.getElementById("btn-rm-border").addEventListener("click", () => {
+		for(const img of gallery.children) {
+			img.style.borderBottom = "none";
+		}
+	});
+
+	document.getElementById("btn-update-width").addEventListener("click", () => {
+		const px = parseFloat(widthInput.value);
+		const img = getCurrentImg();
+
+		if(!px || isNaN(px) || !isFinite(px)) {
+			return alert("Niepoprawna liczba");
+		}
+
+		if(!img) {
+			return alert("Nie wybrałeś zdjęcia");
+		}
+
+		img.style.width = px + "px";
+	});
+
+});
