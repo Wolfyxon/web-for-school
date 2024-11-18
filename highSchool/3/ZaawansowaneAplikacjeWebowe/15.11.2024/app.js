d9968284e262a1b9a51adcee3526150007622253
Wolfyxon <wolfyxon@gmail.com>
2024-11-18 10:06:31 +0100

    Writing

--- highSchool/3/ZaawansowaneAplikacjeWebowe/15.11.2024/app.js
+++ highSchool/3/ZaawansowaneAplikacjeWebowe/15.11.2024/app.js
@@ -7,3 +7,29 @@ console.log(`-= Zawartość ${dir}: =-`);
 for (const sub of fs.readdirSync(dir)) {
     console.log(" ", sub);
 }
+
+class Car {
+    constructor(brand, model, year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year; 
+    }
+
+    toString() {
+        return `${this.brand} ${this.model} ${this.year}`;
+    }
+}
+
+const cars = [
+    new Car("Toyota", "Yaris", 2019),
+    new Car("Mazda", "Miata", 2016),
+    new Car("Ford", "Focus", 210)
+];
+
+let content = "";
+
+for(const car of cars) {
+    content += car + "\n";
+}
+
+fs.writeFileSync(dir + "marka.txt", content);
