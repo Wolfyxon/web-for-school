d835d390d5a2149ea8da9e6f49c15031fc429963
Wolfyxon <wolfyxon@gmail.com>
2024-11-19 13:30:08 +0100

    Finished task 1

--- highSchool/3/ZaawansowaneAplikacjeWebowe/19.11.2024/zad1/script.js
+++ highSchool/3/ZaawansowaneAplikacjeWebowe/19.11.2024/zad1/script.js
@@ -1,3 +1,10 @@
 $(window).ready(() => {
+    function r() {
+        return Math.random() * 255;
+    }
 
+   $("button").click(() => {
+        const color = `rgb(${r()}, ${r()}, ${r()})`;
+        $("body").css("background", color)
+   }); 
 });
