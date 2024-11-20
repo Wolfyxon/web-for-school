0c3c2a2c4731b6aa0142f0f9d29df5ca180386d8
Wolfyxon <wolfyxon@gmail.com>
2024-11-20 09:26:49 +0100

    Implemented query

--- highSchool/3/PracowniaZaawansowanychAplikacjiWebowych/20.11.2024/app.js
+++ highSchool/3/PracowniaZaawansowanychAplikacjiWebowych/20.11.2024/app.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 
 async function input(query) {
     return new Promise((res) => {
+        process.stdout.write(query + ": ");
         process.stdin.resume();
 
         process.stdin.once("data", (buf) => {
