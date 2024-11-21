cfcf17e2be45c2c9545a65dd00d11e6f923eba10
Wolfyxon <wolfyxon@gmail.com>
2024-11-21 07:26:18 +0100

    Counting points

--- highSchool/3/PracowniaAplikacjiInternetowych/21.11.2024/skrypt.js
+++ highSchool/3/PracowniaAplikacjiInternetowych/21.11.2024/skrypt.js
@@ -1,15 +1,21 @@
 window.addEventListener("load", () => {
     const game = document.getElementById("gra");
+    const pointsDisp = document.getElementById("punkty");
     const btn = document.querySelector("input");
 
     btn.addEventListener("click", () => {
         let html = "";
-        
+        let points = 0;
+
         for(let i = 0; i < 2; i++) {
-            const src = `${Math.floor(Math.random() * 6 + 1)}.png`;
+            const r = Math.floor(Math.random() * 6 + 1);
+            const src = `${r}.png`;
+
+            points += r;
             html += `<img src='${src}'>`;
         }
 
         game.innerHTML = html;
+        pointsDisp.innerText = points;
     });
 });
