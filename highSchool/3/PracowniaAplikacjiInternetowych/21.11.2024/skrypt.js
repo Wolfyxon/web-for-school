1c264c2c0a997c7f41103321b1ad476fe861ab55
Wolfyxon <wolfyxon@gmail.com>
2024-11-21 07:23:13 +0100

    Random dies

--- highSchool/3/PracowniaAplikacjiInternetowych/21.11.2024/skrypt.js
+++ highSchool/3/PracowniaAplikacjiInternetowych/21.11.2024/skrypt.js
@@ -1,3 +1,15 @@
 window.addEventListener("load", () => {
-    
+    const game = document.getElementById("gra");
+    const btn = document.querySelector("input");
+
+    btn.addEventListener("click", () => {
+        let html = "";
+        
+        for(let i = 0; i < 2; i++) {
+            const src = `${Math.floor(Math.random() * 6 + 1)}.png`;
+            html += `<img src='${src}'>`;
+        }
+
+        game.innerHTML = html;
+    });
 });
