b80aee6c453d7df7501be9c70a74d995b4b6dace
Wolfyxon <wolfyxon@gmail.com>
2024-11-27 19:24:56 +0100

    Init chart

--- highSchool/3/PracowniaAplikacjiInternetowych/21.11.2024/extra/script.js
+++ highSchool/3/PracowniaAplikacjiInternetowych/21.11.2024/extra/script.js
@@ -1,3 +1,20 @@
 window.addEventListener("load", () => {
+    const chart = document.getElementById("chart");
     
+    let chartHtml = "";
+
+    for(let i = 1; i <= 6; i++) {
+        chartHtml += `<div>
+                <img src="img/${i}.png">
+                
+                <div class="bar-container">
+                    <div class="bar" id="bar-${i}">
+                        <div class="value" id="value-${i}">0</div>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+    chart.innerHTML = chartHtml;
 });
