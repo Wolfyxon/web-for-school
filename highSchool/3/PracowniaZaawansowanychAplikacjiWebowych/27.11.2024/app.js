f73185c8b000e102ee906e5a38422e699325f8c0
Wolfyxon <wolfyxon@gmail.com>
2024-11-27 09:13:31 +0100

    Checking if dir exists

--- highSchool/3/PracowniaZaawansowanychAplikacjiWebowych/27.11.2024/app.js
+++ highSchool/3/PracowniaZaawansowanychAplikacjiWebowych/27.11.2024/app.js
@@ -1,4 +1,17 @@
-// somehow I installed the wrong node lol
-const fs = await import("fs");
-const path = await import("path");
+async function main() {
+    // somehow I installed the wrong node lol
+    const fs = await import("fs");
+    const path = await import("path");
 
+    const move = "move.txt";
+    const dir = "scripts";
+
+    if(!await fs.existsSync(dir)) {
+        return console.log(`Folder ${dir} nie istnieje`);
+    }
+
+    await fs.renameSync("rename.txt", "renamed.txt");
+    await fs.renameSync(move, path.join(dir, "move.txt"));
+}
+
+main();
