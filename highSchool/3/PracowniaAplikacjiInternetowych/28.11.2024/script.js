290ac2dd41ab0529720ebeddd438702b119e8b80
Wolfyxon <wolfyxon@gmail.com>
2024-11-28 07:40:37 +0100

    Init script

--- highSchool/3/PracowniaAplikacjiInternetowych/28.11.2024/script.js
+++ highSchool/3/PracowniaAplikacjiInternetowych/28.11.2024/script.js
@@ -1,3 +1,31 @@
 window.addEventListener("load", () => {
+    const input = document.getElementById("msg-input");
+    const chat = document.getElementById("chat");
 
+    function addMsg(icon, author, text) {
+        const msg = document.createElement("div");
+
+        msg.innerHTML = `
+            <img src="${icon}">
+            <div class="msg-text">
+                <div class="msg-sender">${author}</div>
+                <div class="msg-content">${text}</div>
+            </div>
+        `
+
+        chat.append(msg);
+    }
+
+    function send() {
+        if(input.value.replace(" ", "") == "") return;
+
+        addMsg("", "Ty", input.value);
+        input.value = "";
+    }
+
+    input.addEventListener("keydown", (e) => {
+        if(e.key == "Enter") send();
+    });
+
+    document.getElementById("send-btn", send);
 });
