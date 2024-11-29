3c982d84b4327f15e9d520f9452f3e3f81ce813b
Wolfyxon <wolfyxon@gmail.com>
2024-11-29 15:34:29 +0100

    Init ProgramowanieAplikacjiInternetowych 29.11.2024

---
+++ highSchool/3/ProgramowanieAplikacjiInternetowych/29.11.2024/endpoint.php
@@ -0,0 +1,58 @@
+<?php
+function data($field) {
+    if(!isset($_POST[$field])) {
+        return "Brak";
+    }
+
+    return $_POST[$field];
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div>
+            <p>Rejestracja przebiegła pomyślnie</p>
+
+            <div>
+                <span>Nazwisko: </span> 
+                <span><?php echo data("secondName"); ?></span>
+            </div>
+            <div>
+                <span>Imię: </span> 
+                <span><?php echo data("firstName"); ?></span>
+            </div>
+            <div>
+                <span>Zawód: </span> 
+                <span><?php echo data("job"); ?></span>
+            </div>
+            <div>
+                <span>E-mail: </span> 
+                <span><?php echo data("email"); ?></span>
+            </div>
+            <div>
+                <span>Wykształcenie: </span> 
+                <span><?php echo data("education"); ?></span>
+            </div>
+
+            <p>Znajomość języków:</p>
+            
+            <?php
+                if(isset($_POST["langs"] && count($_POST["langs"]) != 0)) {
+                    echo "<ul>";
+
+                    foreach ($_POST["langs"] as $k => $v) {
+                        echo "<li>" . $v . "</li>";
+                    }
+    
+                    echo "</ul>"
+                }
+
+            ?>
+        </div>
+    </body>
+</html>
