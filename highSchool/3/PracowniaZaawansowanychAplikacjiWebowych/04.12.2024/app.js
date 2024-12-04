16bef54d913c0668b6251e2ac2a0df9221d0d1ac
Wolfyxon <wolfyxon@gmail.com>
2024-12-04 09:15:15 +0100

    Init checks

--- highSchool/3/PracowniaZaawansowanychAplikacjiWebowych/04.12.2024/app.js
+++ highSchool/3/PracowniaZaawansowanychAplikacjiWebowych/04.12.2024/app.js
@@ -1,1 +1,21 @@
 const fs = require("fs");
+const path = require("path");
+
+const file = "copy.txt";
+const dir = "dir";
+const newPath = path.join(file, dir);
+
+function assert(condition, message) {
+    if(!condition) {
+        console.log(message);
+        process.exit(1);
+    }
+}
+
+async function main() {    
+    assert(await fs.existsSync(file), "Źródłowy plik nie istnieje");
+    assert(await fs.existsSync(dir), "Folder docelowy nie istnieje");
+    assert(!(await fs.existsSync(newPath)), "Plik już został skopiowany");
+}
+
+main();
