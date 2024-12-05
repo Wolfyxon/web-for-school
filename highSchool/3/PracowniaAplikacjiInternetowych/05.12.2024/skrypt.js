951e432fd285199910b630c8f6640ef2c490343b
Wolfyxon <wolfyxon@gmail.com>
2024-12-05 07:22:56 +0100

    Added addRow()

--- highSchool/3/PracowniaAplikacjiInternetowych/05.12.2024/skrypt.js
+++ highSchool/3/PracowniaAplikacjiInternetowych/05.12.2024/skrypt.js
@@ -1,3 +1,18 @@
 window.addEventListener("load", () => {
     const table = document.getElementById("table");
+
+    function addRow(methodName, description) {
+        const tr = document.createElement("tr");
+        const code = `"Ala ma kota".${methodName}()`;
+
+        tr.innerHTML = `
+            <td>string.${methodName}()</td>
+            <td>${description}</td>
+            <td>${code}</td>
+            <td>${eval(code)}</td>
+        `;
+
+        table.append(tr);
+    }
+
 });
