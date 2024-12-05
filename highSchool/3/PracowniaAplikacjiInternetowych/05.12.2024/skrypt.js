91eaf61478118f8c3ce4d7e03ea82feb989829d0
Wolfyxon <wolfyxon@gmail.com>
2024-12-05 07:46:32 +0100

    Added all required methods

--- highSchool/3/PracowniaAplikacjiInternetowych/05.12.2024/skrypt.js
+++ highSchool/3/PracowniaAplikacjiInternetowych/05.12.2024/skrypt.js
@@ -41,5 +41,9 @@ window.addEventListener("load", () => {
     addRow("substr", "Zwraca wycinek ciągu znaków. Deprecated.", [1, 5]);
     addRow("substring", "Zwraca wycinek ciągu znaków między 2 indeksami.", [1, 6]);
     addRow("slice", "Działa podobnie do substring", [1, 6]);
-    
+    addRow("split", "Rozdziela ciąg", ["'a'"]);
+    addRow("concat", "Łączy 2 ciągi", ["' bo tak'"]);
+    addRow("replace", "Podmienia ciągi w ciągu na inne", ["'Ala'", "'Basia'"]);
+    addRow("trim", "Usuwa spacje i znak nowel linii na końcu ciągu"); // TODO: Separate example string
+    addRow("charAt", "Zwraca znak na danym indeksie z ciągu", [4]);
 });
