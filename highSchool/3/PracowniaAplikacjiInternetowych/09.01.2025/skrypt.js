eb06f14a57596cfb11d3838d44417503e8cf570f
Wolfyxon <wolfyxon@gmail.com>
2025-01-09 07:37:45 +0100

    Placing pacman

--- highSchool/3/PracowniaAplikacjiInternetowych/09.01.2025/skrypt.js
+++ highSchool/3/PracowniaAplikacjiInternetowych/09.01.2025/skrypt.js
@@ -4,12 +4,19 @@ window.addEventListener("load", () => {
 	const mapW = 3;
 	const mapH = 3;
 
+	const emptyImg = "bialy.jpg"
+
+	let pacman = {
+		x: 0,
+		y: 0
+	};
+	
 	function initMap() {
 		let mapHtml = "";
 
 		for(let y = 0; y < mapH; y++) {
 			for(let x = 0; x < mapW; x++) {
-				mapHtml += "<img src='bialy.jpg'>";
+				mapHtml += `<img src='${emptyImg}'>`;
 			}
 
 			mapHtml += "<br>";
@@ -22,5 +29,21 @@ window.addEventListener("load", () => {
 		return map.children[y * mapW + x];
 	}
 
+	function setPacman(x, y) {
+		const img = "pacman.jpg";
+		
+		for(const tile of map.children) {
+			if(tile.src == img) {
+				tile.src = emptyImg;
+			}
+		}
+
+		getTile(x, y).src = img;
+		
+		pacman.x = x;
+		pacman.y = y;
+	}
+
 	initMap();
+	setPacman(pacman.x, pacman.y);
 });
