0d85f829588fea94f2cc55e9728661cb4eb09fd9
Wolfyxon <wolfyxon@gmail.com>
2025-01-09 07:31:29 +0100

    Map is automatically generated

--- highSchool/3/PracowniaAplikacjiInternetowych/09.01.2025/skrypt.js
+++ highSchool/3/PracowniaAplikacjiInternetowych/09.01.2025/skrypt.js
@@ -1,3 +1,22 @@
 window.addEventListener("load", () => {
+	const map = document.getElementById("mapa");
 
+	const mapW = 3;
+	const mapH = 3;
+
+	function initMap() {
+		let mapHtml = "";
+
+		for(let y = 0; y < mapH; y++) {
+			for(let x = 0; x < mapW; x++) {
+				mapHtml += "<img src='bialy.jpg'>";
+			}
+
+			mapHtml += "<br>";
+		}
+		
+		map.innerHTML = mapHtml
+	}
+
+	initMap();
 });
