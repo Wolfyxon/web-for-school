21ae06c705ecfa5e7449f3bb48b92fb5295d9add
Wolfyxon <wolfyxon@gmail.com>
2025-01-09 07:24:26 +0100

    My teacher's code is garbage

--- highSchool/3/PracowniaAplikacjiInternetowych/09.01.2025/skrypt.js
+++ highSchool/3/PracowniaAplikacjiInternetowych/09.01.2025/skrypt.js
@@ -1,32 +1,3 @@
-let pozycjaPacmana=0;
-let pozycjaWisni=0;
+window.addEventListener("load", () => {
 
-function losujWisnie(){
-	do{
-		pozycjaWisni=Math.floor(9*Math.random());
-	}while(pozycjaWisni==pozycjaPacmana);
-	document.images[pozycjaWisni].src='wisnie.jpg';
-}
-losujWisnie();
-function f1(event){
-	const klawisz=event.keyCode;
-	if(klawisz==39){
-		if(pozycjaPacmana!=2 && pozycjaPacmana!=5 && pozycjaPacmana!=8){
-			pozycjaPacmana++;
-			/*umieszczenie obrazu pacmana tam, gdzie wskazuje zmienna pozycjaPacmana*/
-			document.images[pozycjaPacmana].src='pacman.jpg';
-			/*wyczyszczenie poprzedniej pozycji pacmana*/
-			document.images[pozycjaPacmana-1].src='bialy.jpg';    
-		}
-	}
-	if(klawisz==37){
-		if(pozycjaPacmana!=0 && pozycjaPacmana!=3 && pozycjaPacmana!=6){
-			pozycjaPacmana--;
-			document.images[pozycjaPacmana].src='pacman.jpg';
-			document.images[pozycjaPacmana+1].src='bialy.jpg';       
-		}
-	}
-	if(pozycjaPacmana==pozycjaWisni){
-		setTimeout('losujWisnie()',100);
-	}
-}
+});
