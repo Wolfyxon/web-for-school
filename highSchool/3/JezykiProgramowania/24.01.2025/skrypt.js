34793aad5abf6c52fc0ad5562e5e67fef4516434
Wolfyxon <wolfyxon@gmail.com>
2025-01-24 10:38:51 +0100

    Init image handling

--- highSchool/3/JezykiProgramowania/24.01.2025/skrypt.js
+++ highSchool/3/JezykiProgramowania/24.01.2025/skrypt.js
@@ -1,5 +1,30 @@
-window.addEventListener("load", () => {
-    const gallery = document.getElementById("gallery");
 
+function image(id, callback) {
+    const img = document.getElementById(id + "-img");
+    const main = document.getElementById(id);
     
+    for(const btn of main.getElementsByTagName("button")) {
+        btn.addEventListener("click", () => {
+            callback(img, btn);
+        });
+    }
+    
+}
+
+window.addEventListener("load", () => {
+    image("bee", (img) => {
+
+    });
+
+    image("orange", (img) => {
+
+    });
+
+    image("fruits", (img, btn) => {
+
+    });
+    
+    image("tutel", (img) => {
+        
+    });
 });
