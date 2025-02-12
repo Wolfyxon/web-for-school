bbc86e4ae32f175d07cc2a63371f0870582926c3
Wolfyxon <wolfyxon@gmail.com>
2025-02-12 07:36:53 +0100

    Implemented js

--- highSchool/3/JezykiProgramowania/12.02.2025/script.js
+++ highSchool/3/JezykiProgramowania/12.02.2025/script.js
@@ -1,3 +1,34 @@
 window.addEventListener("load", () => {
+    const inp = document.getElementById("age-inp");
 
+    class RN {
+        constructor(name, min) {
+            this.name = name;
+            this.min = min;
+        }
+    }    
+
+    const ranges = [
+        new RN("Niemowlę", 0),
+        new RN("Dziecko", 2),
+        new RN("Nastolatek", 11),
+        new RN("Student", 18),
+        new RN("Średni", 26),
+        new RN("Podeszły", 50),
+        new RN("Stary", 70)
+    ];
+
+    document.getElementById("check-btn").addEventListener("click", () => {
+        const val = parseInt(inp.value);
+        if(isNaN(val)) return;
+
+        for(let i = 0; i < ranges.length; i++) {
+            const range = ranges[ranges.length - 1 - i];
+
+            if(range.min <= val) {
+                alert(range.name);
+                break;
+            }
+        }
+    });
 });
