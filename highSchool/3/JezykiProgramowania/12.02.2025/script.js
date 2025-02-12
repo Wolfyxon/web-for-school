5dddb32d68a609fe7e39038b42346a28b2ae302a
Wolfyxon <wolfyxon@gmail.com>
2025-02-12 07:41:25 +0100

    Added colors

--- highSchool/3/JezykiProgramowania/12.02.2025/script.js
+++ highSchool/3/JezykiProgramowania/12.02.2025/script.js
@@ -2,20 +2,21 @@ window.addEventListener("load", () => {
     const inp = document.getElementById("age-inp");
 
     class RN {
-        constructor(name, min) {
+        constructor(name, min, color) {
             this.name = name;
             this.min = min;
+            this.color = color;
         }
     }    
 
     const ranges = [
-        new RN("Niemowlę", 0),
-        new RN("Dziecko", 2),
-        new RN("Nastolatek", 11),
-        new RN("Student", 18),
-        new RN("Średni", 26),
-        new RN("Podeszły", 50),
-        new RN("Stary", 70)
+        new RN("Niemowlę", 0, "#90ff36"),
+        new RN("Dziecko", 2, "#366fff"),
+        new RN("Nastolatek", 11, "#132c6e"),
+        new RN("Student", 18, "#841fff"),
+        new RN("Średni", 26, "#ffbf00"),
+        new RN("Podeszły", 50, "#ff7700"),
+        new RN("Stary", 70, "#9e1800")
     ];
 
     document.getElementById("check-btn").addEventListener("click", () => {
@@ -26,6 +27,7 @@ window.addEventListener("load", () => {
             const range = ranges[ranges.length - 1 - i];
 
             if(range.min <= val) {
+                inp.style.color = range.color;
                 alert(range.name);
                 break;
             }
