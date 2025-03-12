d8c71ad0df7fbbac41f3c1810d1e309c1f5abbc7
Wolfyxon <wolfyxon@gmail.com>
2025-03-12 09:11:08 +0100

    Added Wybor column

--- highSchool/3/PracowniaZaawansowanychAplikacjiWebowych/12.03.2025/src/components/Ksiazka.tsx
+++ highSchool/3/PracowniaZaawansowanychAplikacjiWebowych/12.03.2025/src/components/Ksiazka.tsx
@@ -1,9 +1,12 @@
+import Wybor from "./Wybor";
+
 export default function Ksiazka(data: {autor: string, tytul: string, rokWydania: number}) {
     return (
         <tr>
             <td>{data.autor}</td>
             <td>{data.tytul}</td>
             <td>{data.rokWydania}</td>
+            <td><Wybor /></td>
         </tr>
     )
 }
