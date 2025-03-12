37db46e4f8c456fdbb47fa01b08c3ca35b8e94de
Wolfyxon <wolfyxon@gmail.com>
2025-03-12 09:08:14 +0100

    More books

--- highSchool/3/PracowniaZaawansowanychAplikacjiWebowych/12.03.2025/src/components/Ksiegarnia.tsx
+++ highSchool/3/PracowniaZaawansowanychAplikacjiWebowych/12.03.2025/src/components/Ksiegarnia.tsx
@@ -4,6 +4,10 @@ export default function Ksiegarnia() {
     return (
         <table>
             <Ksiazka autor="Rafał Kosik" tytul="Felix Net i Nika oraz Gang Niewidzialnych Ludzi" rokWydania="2004" />
+            <Ksiazka autor="Szatan" tytul="MATeMAtyka 2. Podręcznik. Zakres podstawowy." rokWydania="2024" />
+            <Ksiazka autor="Majonez" tytul="Kolekcja Majonezów" rokWydania="2000" />
+            <Ksiazka autor="George Orwell" tytul="1984" rokWydania="1949" />
+            <Ksiazka autor="Nwm" tytul="Aaaaaaaaaa" rokWydania="2025" />
         </table>
     )
 }
