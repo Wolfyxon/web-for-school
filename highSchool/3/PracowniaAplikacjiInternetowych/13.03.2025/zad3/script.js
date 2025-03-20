6912d34a41468c8db91aeb071f84b118529bca72
Wolfyxon <wolfyxon@gmail.com>
2025-03-20 07:42:13 +0100

    Script

--- highSchool/3/PracowniaAplikacjiInternetowych/13.03.2025/zad3/script.js
+++ highSchool/3/PracowniaAplikacjiInternetowych/13.03.2025/zad3/script.js
@@ -1,3 +1,24 @@
 window.addEventListener("load", () => {
+    const chks = [...document.querySelectorAll("input[type='checkbox']")];
+    const blocks = document.querySelectorAll("#result > *");
+    const form = document.getElementById("form");
     
+    function hideAll() {
+        for(const b of blocks) {
+            b.style.display = "none";
+        }
+    }
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        hideAll();
+
+        for(const ch of chks) {
+            const idx = chks.indexOf(ch);
+            
+            blocks[idx].style.removeProperty(idx);
+        }
+    });
+
+    hideAll();
 });
