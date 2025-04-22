ed6f9c8bdfe460843413df8b0cd2491cdbdd420f
Wolfyxon <wolfyxon@gmail.com>
2025-04-22 16:29:54 +0200

    Init structure

---
+++ highSchool/3/ProgramowanieAplikacjiInternetowych/27.03.2025-exams/Zad08_styczen2025_INF03_PHP/index.php
@@ -0,0 +1,48 @@
+<?php 
+    $db = mysqli_connect("localhost", "root", null, "mieszalnia");
+    mysqli_set_charset($db, "utf8");
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <title>Mieszalnia farb</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <header>
+            <img src="baner.png" alt="Mieszalnia farb">
+        </header>
+
+        <form target="_self" method="post">
+            <label for="date-from">Data odbioru od: </label>
+            <input type="date" name="date-from" id="date-from">
+
+            <label for="date-to">do: </label>
+            <input type="date" name="date-to" id="date-to">
+
+            <input type="submit" value="Wyszukaj">
+        </form>
+        
+        <div id="main">
+            <table>
+                <tr>
+                    <th>Nr. zamówienia</th>
+                    <th>Nazwisko</th>
+                    <th>Imię</th>
+                    <th>Kolor</th>
+                    <th>Pojemność [ml]</th>
+                    <th>Data odbioru</th>
+                </tr>
+                <?php
+                    // TODO
+                ?>
+            </table>
+        </div>
+
+        <footer>
+            <h3>Egzamin INF.03</h3>
+            <p>Autor: 00000000</p>
+        </footer>
+    </body>
+</html>
