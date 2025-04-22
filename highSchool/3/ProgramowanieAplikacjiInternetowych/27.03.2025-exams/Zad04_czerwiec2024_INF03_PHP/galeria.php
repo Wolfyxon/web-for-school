26d676baed66d4824b669ea80a00a7699473a5ec
Wolfyxon <wolfyxon@gmail.com>
2025-04-22 10:55:01 +0200

    Init scripts

---
+++ highSchool/3/ProgramowanieAplikacjiInternetowych/27.03.2025-exams/Zad04_czerwiec2024_INF03_PHP/galeria.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <title>Galeria</title>
+        
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="styl.css">
+    </head>
+    <body>
+        <header id="banner">
+            <h1>Tematy zdjęć</h1>
+        </header>
+
+        <div id="main">
+            <section id="left">
+                <h2>Tematy zdjęć</h2>
+                <ol>
+                    <li>Zwierzęta</li>
+                    <li>Krajobrazy</li>
+                    <li>Miasta</li>
+                    <li>Przyroda</li>
+                    <li>Samochody</li>
+                </ol>
+            </section>
+
+            <section id="center">
+                <?php
+                    echo "TODO"
+                ?>
+            </section>
+
+            <section id="right">
+            <h2>Najbardziej lubiane</h2>
+                <?php
+                    echo "TODO"
+                ?>
+                <strong>Zobacz wszystkie nasze zdjęcia</strong>
+            </section>
+        </div>
+
+        <footer>
+            <h5>Stronę wykonał: 00000000000</h5>
+        </footer>
+    </body>
+</html>
