52540db4d725877e410212e1ac641c2391ee6ca1
Wolfyxon <wolfyxon@gmail.com>
2025-06-03 10:23:32 +0200

    Finished arkusz6

--- highSchool/3/testExams/php03.06.2025/Arkusz6/index.php
+++ highSchool/3/testExams/php03.06.2025/Arkusz6/index.php
@@ -1,3 +1,7 @@
+<?php
+    $db = new mysqli("localhost", "root", null, "obuwie");
+?>
+
 <!DOCTYPE HTML>
 <html lang="pl-PL">
     <head>
@@ -12,27 +16,58 @@
         <main>
             <form method="post" class="kontrolki" action="zamow.php">
                 <label>Model:
-                    <select>
-                        <!--skrypt1-->
+                    <select name="model">
+                        <?php 
+                            $res = $db->query("SELECT model FROM produkt");
+                            
+                            while($row = $res->fetch_assoc()) {
+                                
+                                echo "<option>" . $row["model"] . "</option>";
+                            }
+                        ?>
                     </select>
                 </label>
                 <label>Rozmiar:
-                    <select>
+                    <select name="size">
                         <option>40</option>
                         <option>41</option>
                         <option>42</option>
                         <option>43</option>
                     </select>
                 </label>
-                <label>Liczba par:
-                        <input type="number">
+                <label>
+                        Liczba par:
+                        <input type="number" name="pairs">
                 </label>
                 <button>Zamów</button>
             </form>
-            <!--skrypt2-->
+            
+            <?php
+                $res = $db->query("SELECT buty.model, nazwa, cena, nazwa_pliku FROM buty JOIN produkt ON produkt.model = buty.model");
+
+                while($row = $res->fetch_assoc()) {
+                    $img = $row["nazwa_pliku"];
+                    $name = $row["nazwa"];
+                    $model = $row["model"];
+                    $cost = $row["cena"];
+
+                    echo "<div class='buty'>
+                            <img src='$img' alt='but męski'>
+
+                            <h2>$name</h2>
+                            <h5>Model: $model</h5>
+                            <h4>Cena: $cost</h4>
+
+                          </div>";
+                }
+            ?>
         </main>
         <footer>
             <p>Autor strony: XYZ</p>
         </footer>
     </body>
 </html>
+
+<?php
+    $db->close();
+?>
--- highSchool/3/testExams/php03.06.2025/Arkusz6/zamow.php
+++ highSchool/3/testExams/php03.06.2025/Arkusz6/zamow.php
@@ -11,7 +11,45 @@
         </header>
         <main>
             <h2>Zamówienie</h2>
-            <!--skrypt3-->
+            
+            <?php
+                $model = $_POST["model"] ?? null;
+                $size = $_POST["size"] ?? null;
+                $pairs = $_POST["pairs"] ?? 1;
+
+                if(!$model || !$size) {
+                    return;
+                }
+
+                $db = new mysqli("localhost", "root", null, "obuwie");
+
+                $stm = $db->prepare("SELECT nazwa, cena, kolor, kod_produktu, material, nazwa_pliku FROM produkt 
+                                     JOIN buty ON buty.model = produkt.model WHERE produkt.model = ?
+                                    ");
+                
+                $stm->execute([$model]);
+                $row = $stm->get_result()->fetch_assoc();
+
+                if($row) {
+                    $img = $row["nazwa_pliku"];
+                    $name = $row["nazwa"];
+                    $total_cost = $row["cena"] * $pairs;
+                    $color = $row["kolor"];
+                    $material = $row["material"];
+
+                    echo "<img src='$img'>
+                          <h2>$name</h2>
+                          <p>cena za $pairs par: $total_cost zł</p>
+                          <p>Szczegóły produktu: $color, $material</p>
+                          <p>Rozmiar: $size</p>
+                    ";
+                } else {
+                    echo "Nieznany model";
+                }
+
+                $db->close();
+            ?>
+
             <a href="index.php">Strona główna</a>
         </main>
         <footer>
