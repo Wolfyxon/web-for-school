9aa149773c0c02907b9e54cbd287b87bdaa6cd50
Wolfyxon <wolfyxon@gmail.com>
2025-06-03 09:10:54 +0200

    Imported Arkusz8

---
+++ highSchool/3/testExams/php03.06.2025/Arkusz8/logowanie.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8">
+        <title>Forum o psach</title>
+        <link rel="stylesheet" href="styl5.css">
+    </head>
+    <body>
+        <section class="baner">
+            <h1>Forum wielbicieli psów</h1>
+        </section>
+        <section class="lewy">
+            <img src="obraz.jpg" alt="foksterier">
+        </section>
+        <section class="prawy1">
+            <h2>Zapisz się</h2>
+            <form method="post">
+                <label>login: <input name="login"></label><br>
+                <label>hasło: <input type="password" name="haslo"></label><br>
+                <label>powtórz hasło: <input type="password" name="powtorzHaslo"></label>
+                <button type="submit">Zapisz</button>
+            </form>
+            <!--skrypt-->
+        </section>
+        <section class="prawy2">
+            <h2>Zapraszamy wszystkich</h2>
+            <ol>
+                <ul>właścicieli psów</ul>
+                <ul>weterynarzy</ul>
+                <ul>tych, co chcą kupić psa</ul>
+                <ul>tych, co lubią psy</ul>
+            </ol>
+            <a href="regulamin.html">Przeczytaj regulamin forum</a>
+        </section>
+        <footer>
+            Stronę wykonał: XYZ
+        </footer>
+    </body>
+</html>
