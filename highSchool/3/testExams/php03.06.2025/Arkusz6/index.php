cd93a57632fa1b7bac64b8cfc72258ca05807512
Wolfyxon <wolfyxon@gmail.com>
2025-06-03 09:51:37 +0200

    Imported Arkusz6 and added queries

---
+++ highSchool/3/testExams/php03.06.2025/Arkusz6/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8">
+        <title>Obuwie</title>
+        <link rel="stylesheet" type="text/css" href="style1.css">
+    </head>
+    <body>
+        <header>
+            <h1>Obuwie męskie</h1>
+        </header>
+        <main>
+            <form method="post" class="kontrolki" action="zamow.php">
+                <label>Model:
+                    <select>
+                        <!--skrypt1-->
+                    </select>
+                </label>
+                <label>Rozmiar:
+                    <select>
+                        <option>40</option>
+                        <option>41</option>
+                        <option>42</option>
+                        <option>43</option>
+                    </select>
+                </label>
+                <label>Liczba par:
+                        <input type="number">
+                </label>
+                <button>Zamów</button>
+            </form>
+            <!--skrypt2-->
+        </main>
+        <footer>
+            <p>Autor strony: XYZ</p>
+        </footer>
+    </body>
+</html>
---
+++ highSchool/3/testExams/php03.06.2025/Arkusz6/zamow.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8">
+        <title>Obuwie</title>
+        <link rel="stylesheet" type="text/css" href="style1.css">
+    </head>
+    <body>
+        <header>
+            <h1>Obuwie męskie</h1>
+        </header>
+        <main>
+            <h2>Zamówienie</h2>
+            <!--skrypt3-->
+            <a href="index.php">Strona główna</a>
+        </main>
+        <footer>
+            <p>Autor strony: XYZ</p>
+        </footer>
+    </body>
+</html>
