c0c4213a21373e8f327bf9d715e9eb36b5cfc89b
Wolfyxon <wolfyxon@gmail.com>
2025-06-09 11:38:53 +0200

    Init site

---
+++ highSchool/3/testExams/03.06.2025/fromScratch/Arkusz4/wazenie.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <title>Ważenie samochodów ciężarowych</title>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header id="text-header">
+        <h2>
+        Ważenie pojazdów we Wrocławiu
+        </h2>
+    </header>
+    <header id="img-header">
+        <img src="obraz1.png" alt="waga">
+    </header>
+
+    <section id="left">
+        <h2>Lokalizacje wag</h2>
+        <ol>
+            <!-- TODO: skrypt 1 -->
+        </ol>
+
+        <h2>Kontakt</h2>
+        <a href="mailto:wazenie@wroclaw.pl">napisz</a>
+    </section>
+    
+    <section id="middle">
+        <h2>Alerty</h2>
+
+        <table>
+            <tr>
+                <th>rejestracja</th>
+                <th>ulica</th>
+                <th>waga</th>
+                <th>dzień</th>
+                <th>czas</th>
+            </tr>
+            <!-- TODO: skrypt 2 -->
+        </table>
+
+        <!-- TODO: skrypt 3 -->
+    </section>
+    
+    <section id="right">
+        <img src="obraz2.jpg" alt="tir">
+    </section>
+    
+    <footer>
+        <p>Stronę wykonał: 00000000000000000000</p>
+    </footer>
+</body>
+</html>
