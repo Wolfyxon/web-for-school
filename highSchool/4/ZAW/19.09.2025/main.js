0f6c230f64753183031e2ddf248c2b41ad2fdf23
Wolfyxon <wolfyxon@gmail.com>
2025-09-19 09:28:32 +0200

    Server

--- highSchool/4/ZAW/19.09.2025/main.js
+++ highSchool/4/ZAW/19.09.2025/main.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("<img src='https://media.tenor.com/fIaezRSZPSAAAAAe/cat-explosion.png' /><br/>AAAAAAAAAAAAAAA");
+});
+
+app.post("/submit", (req, res) => {
+    res.json({
+        status: "yipee",
+        name: res.body.name,
+        email: res.body.email
+    });
+});
+
+app.listen(8080);
