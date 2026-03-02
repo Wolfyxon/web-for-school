1de461715419140f4b9bbc6df37c3e793cb45e40
Wolfyxon <wolfyxon@gmail.com>
2026-03-02 08:30:07 +0100

    impl logic

--- highSchool/4/B/02.03.2026/src/App.tsx
+++ highSchool/4/B/02.03.2026/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState, type SubmitEvent } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const COURSES = [
@@ -7,6 +8,16 @@ const COURSES = [
 ];
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [courseId, setCourseId] = useState(0);
+
+  function submit(e: SubmitEvent) {
+    e.preventDefault();
+
+    console.log(name);
+    console.log(COURSES[courseId - 1]);
+  }
+  
   return (
     <>
       <h2>Liczba kursów: {COURSES.length}</h2>
@@ -16,14 +27,28 @@ export default function App() {
         }
       </ol>
 
-      <form>
+      <form onSubmit={submit}>
         <div className="form-group">
           <label htmlFor="inp-name">Imię i nazwisko:</label>
-          <input type="text" className="form-control" id="inp-name" />
+          <input 
+            type="text" 
+            className="form-control" 
+            id="inp-name" 
+            onChange={e => {
+              setName(e.target.value);
+            }}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="inp-course-id">Numer kursu:</label>
-          <input type="text" className="form-control" id="inp-course-id" />
+          <input 
+            type="text" 
+            className="form-control" 
+            id="inp-course-id"
+            onChange={e => {
+              setCourseId(parseInt(e.target.value));
+            }} 
+          />
         </div>
         <br />
         <button className="btn btn-primary">Zapisz do kursu</button>
